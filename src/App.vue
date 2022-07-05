8f81bf3b9a015bb9a68b237dcae7cc94f658e80d
weiguohai <2213211962@qq.com>
2022-07-05 22:42:38 +0800

    移动适配

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
 
     <router-view/>
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-gengduo"></i>
+    </div>
+    <van-button type="default">默认按钮</van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import router from './router'
 import store from './store'
 
 import './styles/index.less'
+import './fonts/iconfont.css'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
